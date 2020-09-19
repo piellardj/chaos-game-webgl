3fa8b1a64fd951663c3ae4f2bf5c4d3748d76310
Jérémie Piellard <jeremie.piellard@gmail.com>
2020-09-19 15:30:23 +0200

    Page generation is now written in Typescript to benefit from webpage-template type checking

--- src/generate-page.ts
+++ src/generate-page.ts
@@ -1,8 +1,8 @@
-const path = require("path");
-const PageBuilder = require("webpage-templates").Demopage;
+import * as path from "path";
+import { Demopage } from "webpage-templates";
 
 const DEST_DIR = path.resolve(__dirname, "..", "docs");
 const PAGE_DATA_PATH = path.resolve(__dirname, "config", "page-data.json");
 const minified = true;
 
-PageBuilder.build(DEST_DIR, PAGE_DATA_PATH, !minified);
+Demopage.build(DEST_DIR, PAGE_DATA_PATH, !minified);
